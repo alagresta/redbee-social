fffa1b852b03c299b74213367f7e4b418c748ea8
agustin <agu_violero@hotmail.com>
2017-08-22 04:21:25 -0300

    index modify

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Angular, Node and Twitter API' });
+  //res.render('index', { title: 'Angular, Node and Twitter API' });
+	 res.json({ message: 'wellcome to social feed api' }); 
 });
 
 module.exports = router;
