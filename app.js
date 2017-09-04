6e53accf593355ba4926c9368f3e81683b6126c4
agustin <agu_violero@hotmail.com>
2017-09-04 00:07:11 -0300

    index

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 
